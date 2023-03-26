57f0be8d0ebcd443d318eee7228a3d8e82ad3863
pgeeser <pgeeser@student.42heilbronn.de>
2023-03-26 01:24:18 +0100

    changes

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pgeeser <pgeeser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 00:20:50 by pgeeser           #+#    #+#             */
-/*   Updated: 2023/03/26 00:42:38 by pgeeser          ###   ########.fr       */
+/*   Updated: 2023/03/26 01:23:57 by pgeeser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	main(int argc, char **argv)
 		std::cout << "Usage: ./replacer <filename> <s1> <s2>" << std::endl;
 		return (1);
 	}
-
 	Replacer replacer(argv[1]);
 	if (replacer.replace(argv[2], argv[3])) {
 		return (1);
