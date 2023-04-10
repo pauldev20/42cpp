82a16e940780ed05e4cbb80ab98b12572dda96a5
pgeeser <pgeeser@student.42heilbronn.de>
2023-04-10 12:47:30 +0200

    new version

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: pgeeser <pgeeser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 20:37:29 by pgeeser           #+#    #+#             */
-/*   Updated: 2023/04/10 00:13:11 by pgeeser          ###   ########.fr       */
+/*   Updated: 2023/04/10 11:18:48 by pgeeser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,47 +52,34 @@ void				ScalarConverter::convert(std::string const input)
 	double		doubleVal = 0.0;
 
 	if (input.size() == 1 && !std::isdigit(input[0]) && std::isprint(input[0])) {
-		charVal = std::string("'") + input[0] + std::string("'");
-		std::cout << "char: " << charVal << std::endl;
-		std::cout << "int: " << static_cast<int>(input[0]) << std::endl;
-		std::cout << "float: " << static_cast<float>(input[0]) << ".0f" << std::endl;
-		std::cout << "double: " << static_cast<double>(input[0]) << ".0" << std::endl;
-		return;
+		charVal = input[0];
+		intVal = static_cast<int>(input[0]);
+		floatVal = static_cast<float>(input[0]);
+		doubleVal = static_cast<double>(input[0]);
 	} else if (input.find_first_not_of("0123456789") == std::string::npos) {
 		intVal = std::stoi(input);
+		floatVal = std::stof(input);
+		doubleVal = std::stod(input);
 		if (std::isprint(intVal))
 			charVal = std::string("'") + static_cast<char>(intVal) + std::string("'");
 		else
 			charVal = "Non displayable";
-		std::cout << "char: " << charVal << std::endl;
-		std::cout << "int: " << intVal << std::endl;
-		std::cout << "float: " << static_cast<float>(intVal) << ".0f" << std::endl;
-		std::cout << "double: " << static_cast<double>(intVal) << ".0" << std::endl;
-		return;
 	} else if (input[input.size() - 1] == 'f') {
 		floatVal = std::stof(input);
-		intVal = std::stoi(input);
+		intVal = static_cast<int>(floatVal);
+		doubleVal = static_cast<double>(floatVal);
 		if (std::isprint(intVal))
-			charVal = std::string("'") + static_cast<char>(intVal) + std::string("'");
+			charVal = std::string("'") + static_cast<char>(floatVal) + std::string("'");
 		else
 			charVal = "Non displayable";
-		std::cout << "char: " << charVal << std::endl;
-		std::cout << "int: " << intVal << std::endl;
-		std::cout << "float: " << floatVal << "f" << std::endl;
-		std::cout << "double: " << static_cast<double>(floatVal) << std::endl;
-		return;
 	} else if (input.find_first_not_of("0123456789.") == std::string::npos && input.find('.') != std::string::npos) {
 		doubleVal = std::stod(input);
-		intVal = std::stoi(input);
+		intVal = static_cast<int>(doubleVal);
+		floatVal = static_cast<float>(doubleVal);
 		if (std::isprint(intVal))
-			charVal = std::string("'") + static_cast<char>(intVal) + std::string("'");
+			charVal = std::string("'") + static_cast<char>(doubleVal) + std::string("'");
 		else
 			charVal = "Non displayable";
-		std::cout << "char: " << charVal << std::endl;
-		std::cout << "int: " << intVal << std::endl;
-		std::cout << "float: " << static_cast<float>(doubleVal) << "f" << std::endl;
-		std::cout << "double: " << doubleVal << std::endl;
-		return;
 	} else {
 		for (int i = 0; i < 6; i++) {
 			if (input == pseudoLiterals[i]) {
@@ -105,4 +92,8 @@ void				ScalarConverter::convert(std::string const input)
 		}
 		std::cout << "Invalid input" << std::endl;
 	}
+	std::cout << "char: " << charVal << std::endl;
+	std::cout << "int: " << intVal << std::endl;
+	std::cout << "float: " << floatVal << "f" << std::endl;
+	std::cout << "double: " << doubleVal << std::endl;
 }
