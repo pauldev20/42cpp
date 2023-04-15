caa4fb64368614de52a545042be69fd839689df8
pgeeser <pgeeser@student.42heilbronn.de>
2023-04-15 15:26:36 +0200

    changes

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pgeeser <pgeeser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 15:34:20 by pgeeser           #+#    #+#             */
-/*   Updated: 2023/04/07 19:30:03 by pgeeser          ###   ########.fr       */
+/*   Updated: 2023/04/15 13:28:48 by pgeeser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,36 @@
 
 int main(void)
 {
-	Animal* animals[100];
-	for (int i = 0; i < 50; i++)
-	{
-		animals[i] = new Dog();
-		animals[50 + i] = new Cat();
-	}
-
-	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound! 
-	j->makeSound();
-    meta->makeSound();
-
-	const WrongAnimal* wrong_meta = new WrongAnimal();
-	const WrongAnimal* wrong_i = new WrongCat();
-
-	std::cout << wrong_i->getType() << " " << std::endl;
-	wrong_i->makeSound();
-	wrong_meta->makeSound();
-
 	delete j;
 	delete i;
-	
-	for (int i = 0; i < 100; i++)
-		delete animals[i];
 
-	system("leaks animals");
+	std::cout << std::endl;
+	
+	Animal* animals[10];
+	for (int v = 0; v < 5; v++)
+	{
+		animals[v] = new Dog();
+		animals[5 + v] = new Cat();
+	}
+	for (int v = 0; v < 10; v++)
+		delete animals[v];
+
+	std::cout << std::endl;
+
+	{
+		Dog test;
+		Dog tmp;
+		tmp = test;
+		// Dog	tmp2(test);
+
+		tmp.makeSound();
+		// tmp2.makeSound();
+    }
+
+	std::cout << std::endl;
 
 	return (0); 
 }
